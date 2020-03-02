0b68e27cfb45a43380fffeff688e7de62ecf70b9
paul-shannon <paul.thurmond.shannon@gmail.com>
2020-03-02 07:21:02 -0800

    latest

---
+++ lushootseedCharacters.el
@@ -0,0 +1,13 @@
+(defun schwa ()  ;; ə  ə
+   (interactive)
+   ;;(insert "\u0259")
+   (insert (char-from-name "LATIN SMALL LETTER SCHWA"))
+   )
+
+(defun epsilon ()  ;; ε
+   (interactive)
+   (insert (char-from-name "GREEK SMALL LETTER EPSILON"))
+   )
+
+
+
